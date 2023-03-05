94b187ab47c757790f239c502ac67c157c6a1ee3
Coder-SlayerPro <code.slayer101.gmail.com>
2023-03-05 10:50:00 +0530

    added files

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,41 +1,65 @@
+// import React, { useState } from "react";
+
+// const Navbar =()=>{
+
+//         const [isMobile,setIsMobile] = useState(false)
+//   return (
+//     <>
+//    <nav className='navbar'>
+//     <h3 className='logo'>REACT_APP</h3>
+//     <ul className={isMobile ? "nav-link-mobile" : "nav-link"}
+//     onClick={()=> setIsMobile(false)}>
+//       <li className='home'>Home</li>
+//       <li className='about'>About</li>
+//       <li className='profile'>Profile</li>
+//       <li className='contact'>Contact</li>
+//       <li className='details'>Deatils</li>
+//       <li className='signup'>Sign-Up</li>
+//     </ul>
+//     <button className='mobile-menu-icon'>{isMobile ? <i className='fas fa-times'></i> : <i className='fas fa-bars'></i>} </button>
+//    </nav>
+
+//    </>
+//   );
+
+// }
+// export default Navbar;
+
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useStateValue } from "../context";
 
-
-function Navbar(){
-
-    const[{basket}] = useStateValue()
-    const Navigate = useNavigate()
-    return(
-        <div className="container-nav">
-            <div className="Navbar">
-                <div className="logo">
-                    
-                <img src="./amazon-smile.png" onClick={()=> Navigate("/")}/>
-               </div>
-               <div className="search">
-                 <input type='search' placeholder='Search'/>
-               </div>
-               <div className="right-Container">
-                 <div className="col1">
-                    <p className="text-1" onClick={()=>Navigate("/login")}>Hello, Sign in</p>
-                    <p className="text">Guest</p>
-                 </div>
-                 <div className="col1">
-                  <p className="text-1">Return</p>
-                  <p className="text-2">& Order</p>
-                 </div>
-                 <div className="basket-Btn" onClick={()=> Navigate('/checkout')}>
-                     <img src="./basket2.jpg" />
-                     <p className="text" >{basket.length}</p>
-                 </div>
-               </div>
-            </div>
+function Navbar() {
+  const [{ basket }] = useStateValue();
+  const Navigate = useNavigate();
+  return (
+    <div className="container-nav">
+      <div className="Navbar">
+        <div className="logo">
+          <img src="./amazon-smile.png" onClick={() => Navigate("/signup")} />
         </div>
-    )
+        <div className="search">
+          <input type="search" placeholder="Search" />
+        </div>
+        <div className="right-Container">
+          <div className="col1">
+            <p className="text-1" onClick={() => Navigate("/login")}>
+              Hello, Sign in
+            </p>
+            <p className="text">Guest</p>
+          </div>
+          <div className="col1">
+            <p className="text-1">Retur</p>
+            <p className="text-1">& Order</p>
+          </div>
+          <div className="basket-Btn" onClick={() => Navigate("/checkout")}>
+            <img src="./basket2.jpg" />
+            <p className="text">{basket.length}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-
-
-export default Navbar
+export default Navbar;
